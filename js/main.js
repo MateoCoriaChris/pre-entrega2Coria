ad1effaeb0860a47a0b0f1e5f8e04f97a237dcd0
MateoCoriaChris <106635887+MateoCoriaChris@users.noreply.github.com>
2023-01-16 21:46:05 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,102 +49,28 @@ class producto2{
 }
 
 gustos=['chocolate', 'vainilla','granizado','mascarpone','frutilla', 'dulce de leche', 'frutos del bosque', 'tiramisu'];
-variedadesDePasteles=['pastel cheesecake','pastel selva negra','pastel de frutilla'];
-variedadesDeHeladosDeAgua=['helado de agua de naranja','helado de agua de limon','helado de agua de frutilla'];
-
-
-const cucurchoSimple= new producto1('Cucurucho Simple',gustos,300);
-const cucurchoDoble=new producto1('Cucurucho Doble',gustos,380);
-const heladoDeUnCuarto=new producto1('Helado de 1/4kg',gustos,420);
-const heladoDeUnMedio=new producto1('Helado de 1/2kg',gustos,420);
-const heladoDeUnKilo=new producto1('Helado de 1kg',gustos,1260);
+elegirGusto=(gustos)=>{
+      prompt('1.chocolate, 2.vainilla, 3.granizado, 4.mascarpone, 5.frutilla, 6.dulce de leche, 7.frutos del bosque, 8.tiramisu')
+      
+}
 
 const productos = [
-      helados=[cucurchoSimple, cucurchoDoble,heladoDeUnCuarto,heladoDeMedio,heladoDeUnKilo], 
-      pasteles=new producto2(variedadesDePasteles,2500), 
-      heladosDeAgua=new producto2(variedadesDeHeladosDeAgua,200)
+       {helados:[
+              {producto:'Cucurucho Simple', sabores:gustos,precio:300},
+              {producto:'Cucurucho Doble', sabores:gustos,precio:380},
+              {producto:'Helado de 1/4kg', sabores:gustos,precio:420},
+              {producto:'Helado de 1/2kg', sabores:gustos,precio:420},
+              {producto:'Helado de 1kg', sabores:gustos, precio:1260} 
+            ]},
+               
+       {pasteles:[
+            {producto:'pastel cheesecake',precio:2500},
+            {producto:'pastel selva negra',precio:2550},
+            {producto:'pastel de frutilla',precio:2450}
+       ]},
+       {heladosDeAgua:[
+            {producto:'helado de agua de naranja',precio:200},
+            {producto:'helado de agua de limon',precio:200},
+            {producto:'helado de agua de frutilla',precio:200}
+       ]}
 ]
-
-
-
-
-
-do {producto=prompt('¿Que se les ofrece? 1.helados  2.pasteles  3.helados de agua. ingrese "FIN" para salir.');
-        productoNoValido=producto!=='1'||'2'||'3'
-        switch(producto){
-          case 'helados':
-                productoIngresadoA=prompt('¿Que clase de helado va a querer? 1.cucurucho simple   2.cucrucho doble   3.helado de 1/4kg   4.helado de 1/2kg   5.helado de 1kg.');
-                switch(productoIngresadoA){
-                      case 'cucurucho simple':
-                      cucuruchoSimple=alert('el cucurucho simple sale 300$¿Que sabor va a querer en su cucurucho simple?');
-                      Gusto();
-                      pago(300);
-                      break;
-                      case 'cucurucho doble':
-                            cucuruchoDoble=alert('el cucrucho doble sale 380$¿Que sabor va a querer en su cucurucho doble?')
-                            Gusto();
-                            pago(380);
-                            break;
-                      case 'helado de 1/4kg':
-                            heladoDeUnCuarto=alert('el helado de 1/4kg sale 420$¿Que sabor va a querer en su Helado de 1/4?')
-                            Gusto();
-                            pago(420);
-                            break;
-                      case 'helado de 1/2kg':
-                            heladoDeMedio=alert('el helado de 1/2kg sale 780$¿Que sabor va a querer en su Helado de 1/2kg?')
-                            Gusto();
-                            pago(780);
-                            break;
-                      case 'helado de 1kg':
-                            heladoDeUnKilo=prompt('el helado de 1kg sale 1260$¿Que sabor va a querer en su helado de 1kg?')
-                            Gusto();
-                            pago(1260);
-                            break;
-                      default:
-                            alert('El helado ingresado no existe. Ingrese otro tipo de helado');
-                }
-                break;
-          case '2':
-                productoIngresadoB=prompt('¿Que clase de pastel va a querer? 1.cheesecake 2.selva negra 3.pastel de frutilla.');
-                switch(productoIngresadoB){
-                      case'1':
-                      alert('el pastel cheesecake sale 2500$');
-                      pago(2500);
-                      break;
-                      case'2':
-                      alert('el pastel selva negra sale 2550$');
-                      pago(2550);
-                      break;
-                      case'3':
-                      alert('el pastel de frutilla sale 2450$');
-                      pago(2450);
-                      break;
-                      default:
-                            alert('El pastel ingresado no existe. Ingrese otro tipo de pastel');   
-                }
-    
-                break;
-          case '3':
-                prompt('¿Que clase de helado de agua va a querer? 1.de naranja 2.de limon 3.de frutilla.');
-                productoIngresadoC=prompt('¿Que clase de helado de agua va a querer?');
-                switch(productoIngresadoC){
-                      case'30':
-                      alert('el helado de agua de naranja sale 200$');
-                      pago(200);
-                      break;
-                      case'31':
-                      alert('el helado de agua de limon sale 200$');
-                      pago(200);
-                      break;
-                      case'32':
-                      alert('el helado de agua de frutilla sale 200$');
-                      pago(200);
-                      break;
-                      default:
-                            alert('El helado de agua ingresado no existe. Ingrese otro tipo de helado de agua');
-                }
-                break;
-          default:
-                alert('El producto ingresado no existe. Ingrese otro tipode producto o ingrese "FIN" para salir.');   
-        }
-    }while(producto!= 'FIN');
